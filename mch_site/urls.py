6965cc608fd044f7e1f945ad043c663feba304ea
SimpleNotSimpler <simplenotsimpler@outlook.com>
2025-09-20 17:54:56 -0400

    chore: production prep - set up gunicorn

--- mch_site/urls.py
+++ mch_site/urls.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from decouple import config
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # NOTE: if see repeated attempts to /admin, install django-admin-honeypot for better tracking
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    #hide admin url per https://www.digitalocean.com/community/tutorials/how-to-harden-your-production-django-project
+    # hide admin url per https://www.digitalocean.com/community/tutorials/how-to-harden-your-production-django-project
     path(config('ADMIN_URL'), admin.site.urls),
     path('', include('portfolio.urls')),
     path('resumes/', include('resumes.urls'))
@@ -17,6 +18,9 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+    # so static files work when test gunicorn locally
+    # https://stackoverflow.com/questions/12800862/how-to-make-django-serve-static-files-with-gunicorn
+    urlpatterns += staticfiles_urlpatterns()
 
 admin.site.site_header = "MyCareerHub Admin Portal"
 admin.site.site_title = "MyCareerHub Admin Portal"
