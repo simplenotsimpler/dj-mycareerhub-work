f6ccacb791e18cf5fdfd70dfd66e767794b6c667
SimpleNotSimpler <simplenotsimpler@outlook.com>
2025-12-07 12:40:10 -0500

    fix: return projects order pinned on GitHub profile

--- common/github.py
+++ common/github.py
@@ -9,13 +9,14 @@ def get_projects():
     Fetch pinned repositories from GitHub GraphQL API.
 
     Returns:
-        list[dict]: Flattened, sorted pinned repositories.
+        list[dict]: Flattened, repositories in order pinned in GitHub.
     """
     token = config('GH_TOKEN', default=None)
     url = config('GH_URL', default=None)
 
     if not token or not url:
-        raise RuntimeError("Missing GH_TOKEN or GH_URL in environment variables (.env).")
+        raise RuntimeError(
+            "Missing GH_TOKEN or GH_URL in environment variables (.env).")
 
     gh_query = {
         "query": """
@@ -55,7 +56,8 @@ def get_projects():
     }
 
     try:
-        response = requests.post(url, json=gh_query, headers=headers, timeout=3)
+        response = requests.post(
+            url, json=gh_query, headers=headers, timeout=3)
         response.raise_for_status()
     except requests.RequestException as e:
         raise RuntimeError(f"GitHub API request failed: {e}")
@@ -65,24 +67,22 @@ def get_projects():
     except (KeyError, TypeError) as e:
         raise RuntimeError(f"Unexpected GitHub API response: {e}")
 
+    # Because this is list, retains order pinned on GitHub profile
+    # TODO fix openGraphImage does not necessarily load the first time
     projects_with_flat = []
     for project in projects:
-        project["topics"] = [n["topic"]["name"] for n in project["topics"]["nodes"]]
-        project["languages"] = [e["node"]["name"] for e in project["languages"]["edges"]]
+        project["topics"] = [n["topic"]["name"]
+                             for n in project["topics"]["nodes"]]
+        project["languages"] = [e["node"]["name"]
+                                for e in project["languages"]["edges"]]
         projects_with_flat.append(project)
 
     # Adjust Brickyard project manually
+    # still need date in case display date on portfolio
     for project in projects_with_flat:
         if project["name"] == "brickyard-ceramics":
             project["createdAt"] = "2020-06-15"
             project["pushedAt"] = "2021-10-08"
             project["languages"] = ["HTML", "CSS"]
 
-    # Sort by pushedAt descending
-    projects_sorted = sorted(
-        projects_with_flat,
-        key=lambda p: datetime.fromisoformat(p["pushedAt"].replace("Z", "+00:00")),
-        reverse=True,
-    )
-
-    return projects_sorted
+    return projects_with_flat
